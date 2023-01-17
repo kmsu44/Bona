5307e00c6831f515b78fe01c4c1a4e3b7c9dc90e
kmsu44 <kmsu444@naver.com>
2023-01-18 02:57:26 +0900

    Drawing Route

--- src/App.js
+++ src/App.js
@@ -6,17 +6,85 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Note from "./components/Note";
 import Product from "./components/Product";
+import sample from "./img/sample.jpeg";
+import sample2 from "./img/sample2.jpeg";
+import sample3 from "./img/sample3.jpeg";
 
 function App() {
+  const data = [
+    {
+      id: 1,
+      src: sample,
+      title: "새벽의 아침",
+      year: "2022",
+      ingredient: "캔버스에 유채",
+      size: "1024x1024",
+      purchase: "O",
+      text: "이 작품과 같이 중앙 패널과 개폐가 가능한 좌우의 날개 부분으로이루어진 작품을 ”세 폭 제단화(트립틱)”이라고 합니다. 이것은 전패널을 횡단하는 풍경을 배경으로, 중앙에는 “그리스도 십자가형”이, 양날개 부분에는 무릎을 꿇고 기도하는 성화 기증자(기진자) 부부가 그려져있습니다. 클레베는 16세기 전반의 안트워프에서 활약한 화가로, 넓은파노라마 화면 속에 기괴한 암석 등의 세부를 세밀하게 그려 넣은 풍경묘사는 16세기 플랑드르 풍경화의 특색을 잘 보여줍니다.",
+    },
+    {
+      id: 2,
+      src: sample2,
+      title: "도시의 거리",
+      year: "2022",
+      ingredient: "캔버스에 유채",
+      size: "1024x1024",
+      purchase: "O",
+      text: "이 작품과 같이 중앙 패널과 개폐가 가능한 좌우의 날개 부분으로이루어진 작품을 ”세 폭 제단화(트립틱)”이라고 합니다. 이것은 전패널을 횡단하는 풍경을 배경으로, 중앙에는 “그리스도 십자가형”이, 양날개 부분에는 무릎을 꿇고 기도하는 성화 기증자(기진자) 부부가 그려져있습니다. 클레베는 16세기 전반의 안트워프에서 활약한 화가로, 넓은파노라마 화면 속에 기괴한 암석 등의 세부를 세밀하게 그려 넣은 풍경묘사는 16세기 플랑드르 풍경화의 특색을 잘 보여줍니다.",
+    },
+    {
+      id: 3,
+      src: sample3,
+      title: "크리스마스 트리",
+      year: "2022",
+      ingredient: "캔버스에 유채",
+      size: "1024x1024",
+      purchase: "O",
+      text: "이 작품과 같이 중앙 패널과 개폐가 가능한 좌우의 날개 부분으로이루어진 작품을 ”세 폭 제단화(트립틱)”이라고 합니다. 이것은 전패널을 횡단하는 풍경을 배경으로, 중앙에는 “그리스도 십자가형”이, 양날개 부분에는 무릎을 꿇고 기도하는 성화 기증자(기진자) 부부가 그려져있습니다. 클레베는 16세기 전반의 안트워프에서 활약한 화가로, 넓은파노라마 화면 속에 기괴한 암석 등의 세부를 세밀하게 그려 넣은 풍경묘사는 16세기 플랑드르 풍경화의 특색을 잘 보여줍니다.",
+    },
+    {
+      id: 4,
+      src: sample2,
+      title: "도시의 거리",
+      year: "2022",
+      ingredient: "캔버스에 유채",
+      size: "1024x1024",
+      purchase: "O",
+      text: "이 작품과 같이 중앙 패널과 개폐가 가능한 좌우의 날개 부분으로이루어진 작품을 ”세 폭 제단화(트립틱)”이라고 합니다. 이것은 전패널을 횡단하는 풍경을 배경으로, 중앙에는 “그리스도 십자가형”이, 양날개 부분에는 무릎을 꿇고 기도하는 성화 기증자(기진자) 부부가 그려져있습니다. 클레베는 16세기 전반의 안트워프에서 활약한 화가로, 넓은파노라마 화면 속에 기괴한 암석 등의 세부를 세밀하게 그려 넣은 풍경묘사는 16세기 플랑드르 풍경화의 특색을 잘 보여줍니다.",
+    },
+    {
+      id: 5,
+      src: sample2,
+      title: "도시의 거리",
+      year: "2022",
+      ingredient: "캔버스에 유채",
+      size: "1024x1024",
+      purchase: "O",
+      text: "이 작품과 같이 중앙 패널과 개폐가 가능한 좌우의 날개 부분으로이루어진 작품을 ”세 폭 제단화(트립틱)”이라고 합니다. 이것은 전패널을 횡단하는 풍경을 배경으로, 중앙에는 “그리스도 십자가형”이, 양날개 부분에는 무릎을 꿇고 기도하는 성화 기증자(기진자) 부부가 그려져있습니다. 클레베는 16세기 전반의 안트워프에서 활약한 화가로, 넓은파노라마 화면 속에 기괴한 암석 등의 세부를 세밀하게 그려 넣은 풍경묘사는 16세기 플랑드르 풍경화의 특색을 잘 보여줍니다.",
+    },
+    {
+      id: 6,
+      src: sample2,
+      title: "도시의 거리",
+      year: "2022",
+      ingredient: "캔버스에 유채",
+      size: "1024x1024",
+      purchase: "O",
+      text: "이 작품과 같이 중앙 패널과 개폐가 가능한 좌우의 날개 부분으로이루어진 작품을 ”세 폭 제단화(트립틱)”이라고 합니다. 이것은 전패널을 횡단하는 풍경을 배경으로, 중앙에는 “그리스도 십자가형”이, 양날개 부분에는 무릎을 꿇고 기도하는 성화 기증자(기진자) 부부가 그려져있습니다. 클레베는 16세기 전반의 안트워프에서 활약한 화가로, 넓은파노라마 화면 속에 기괴한 암석 등의 세부를 세밀하게 그려 넣은 풍경묘사는 16세기 플랑드르 풍경화의 특색을 잘 보여줍니다.",
+    },
+  ];
   return (
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Drawing" element={<Drawing />} />
+        <Route path="/Drawing" element={<Drawing data={data} />} />
         <Route path="/Note" element={<Note />} />
         <Route path="/Product" element={<Product />} />
-        <Route path="/DrawingDetail" element={<DrawingDetail />} />
+        <Route
+          path="/DrawingDetail/:id"
+          element={<DrawingDetail data={data} />}
+        />
       </Routes>
       <Footer />
     </BrowserRouter>
--- src/components/Drawing.js
+++ src/components/Drawing.js
@@ -1,18 +1,15 @@
 import styled from "./drawing.module.css";
-import sample from "../img/sample.jpeg";
-import sample2 from "../img/sample2.jpeg";
 import { Link } from "react-router-dom";
 function Drawing(props) {
   return (
     <div className={styled.container}>
-      <Link to="/DrawingDetail">
-        <div className={styled.card}>
-          <img src={sample} alt="HomeImg" className={styled.img} />
-        </div>
-      </Link>
-      <div className={styled.card}>
-        <img src={sample2} alt="HomeImg" className={styled.img} />
-      </div>
+      {props.data.map((data, index) => (
+        <Link to={`/DrawingDetail/${data.id}`} key={index}>
+          <div className={styled.card}>
+            <img src={data.src} alt="HomeImg" className={styled.img} />
+          </div>
+        </Link>
+      ))}
     </div>
   );
 }
